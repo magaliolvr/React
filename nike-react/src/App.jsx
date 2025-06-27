14b7060e9f7421b26f38e3d19af8eea48c2aa52d
Mag <maagali.oliveira@gmail.com>
2025-06-27 10:41:35 +0100

    fixed error

--- nike-react/src/App.jsx
+++ nike-react/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Button from "./components/Button";
-import card1 from "./components/card1";
 import Button2 from "./components/Button2";
 import Shape from "./components/Shape";
 
